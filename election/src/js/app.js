a72c80b2e7e0e2ee5e2bea521e0623ec13a0b73f
sener7106 <dskbbsm@gmail.com>
2021-06-14 23:55:19 +0900

    추가

--- election/src/js/app.js
+++ election/src/js/app.js
@@ -1,121 +1,132 @@
+App = {
 
-  App = {
+    web3Provider: null,
+    contracts: {},
+    account: '0x0',
 
-      web3Provider: null,
-      contracts: {},
-      account: '0x0',
+    init: function() {
+        return App.initWeb3();
+    },
 
-      init: function() {
-          return App.initWeb3();
-      },
+    initWeb3: function() {
+        if (typeof web3 !== 'undefined') {
+            // If a web instance is already provided by metamask.
+            // 만약 웹 환경이 이미 메타마스크에 제공되고 있는 경우.
+            App.web3Provider = web3.currentProvider;
+            web3 = new Web3(web3.currentProvider);
+        } else {
+            // Spectify default instance if no web3 instance provided
+            // 인스턴스가 실행되고 있지 않은 경우
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            web3 = new Web3(App.web3Provider);
+        }
+        return App.initContract();
+    },
 
-      initWeb3: function() {
-          if (typeof web3 !== 'undefined') {
-              // If a web instance is already provided by metamask.
-              // 만약 웹 환경이 이미 메타마스크에 제공되고 있는 경우.
-              App.web3Provider = web3.currentProvider;
-              web3 = new Web3(web3.currentProvider);
-          } else {
-              // Spectify default instance if no web3 instance provided
-              // 인스턴스가 실행되고 있지 않은 경우
-              App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-              web3 = new Web3(App.web3Provider);
-          }
-          return App.initContract();
-      },
+    initContract: function() {
+        $.getJSON("Election.json", function(election) {
+            //$("_cmt" + seq)는document.all.("_cmt" + seq) 
+            //Instantiate a new truffle contract from the artifact 
+            //스마트계약 실행
+            App.contracts.Election = TruffleContract(election);
+            // Connect provider to interact with contract
+            App.contracts.Election.setProvider(App.web3Provider);
 
-      initContract: function() {
-          $.getJSON("Election.json", function(election) {
-              //$("_cmt" + seq)는document.all.("_cmt" + seq) 
-              //Instantiate a new truffle contract from the artifact 
-              //스마트계약 실행
-              App.contracts.Election = TruffleContract(election);
-              // Connect provider to interact with contract
-              App.contracts.Election.setProvider(App.web3Provider);
+            //    App.listenForEvents();
 
-              //    App.listenForEvents();
-            
-              return App.render();
-          });
-      },
-
-
-      render: function() {
-          var electionInstance;
-          var container = $(".container");
-          var loader = $("#loader");
-          var content = $("#content");
- 
-          // index.html의 loader와 content와 연결
-          
-          container.show();
-          loader.show();
-          content.hide();
-          // rendering되기 전까지 loader화면은 보이고, content화면은 숨김
-
-          // Load account data
-          web3.eth.getCoinbase(function(err, account) {
-              if (err === null) {
-                  App.account = account;
-                  $("#accountAddress").html("현재 계정 : " + account);
-                  // error가 없을 경우, html에 account주소를 표기
-                  // 이더 계정을 연동
-              }
-
-          });
+            return App.render();
+        });
+    },
 
 
+    render: function() {
+        var electionInstance;
+        var container = $(".container");
+        var loader = $("#loader");
+        var content = $("#content");
 
-          // Load Contract data
+        // index.html의 loader와 content와 연결
 
-          App.contracts.Election.deployed().then(function(instance) {
-              electionInstance = instance;
-              return electionInstance.candidatesCount();
-          }).then(function(candidatesCount) {
-              var candidatesResults = $("#candidatesResults");
-              candidatesResults.empty();
-              // candidatesResult를 index.html에 표시. 
-              var candidatesSelect = $("#candidatesSelect");
-              candidatesSelect.empty();
-              for (var i = 1; i <= candidatesCount; i++) {
+        container.show();
+        loader.show();
+        content.hide();
+        // rendering되기 전까지 loader화면은 보이고, content화면은 숨김
 
-                  electionInstance.candidates(i).then(function(candidate) {
-                      var id = candidate[0];
-                      var name = candidate[1];
-                      var voteCount = candidate[2];
-                     
-                      
-                      // 후보자 수 만큼 electionInstance에 후보자 데이터를 저장
-                      // Render candidate Result
-                      var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>"  + voteCount + "</td></tr>"
-                      candidatesResults.append(candidateTemplate);
-                      // 후보자 템플릿에 id, name, votecount를 추가
+        // Load account data
+        web3.eth.getCoinbase(function(err, account) {
+            if (err === null) {
+                App.account = account;
+                $("#accountAddress").html("현재 계정 : " + account);
+                // error가 없을 경우, html에 account주소를 표기
+                // 이더 계정을 연동
+            }
 
-                      // Render Candidate ballot option
-                      var candidateOption = "<option value='" + id + "' >" + name + "</option>"
-                      candidatesSelect.append(candidateOption);
-                      
-                      //Render candidate Result total
-                  });
-              }
-              return electionInstance.voters(App.account);
-          }).then(function(hasVoted) {
-              //Do not allow a user to Vote
-              if (hasVoted) {
-                  $('form').hide();
-                  
-                  // 투표를 한 경우 폼을 삭제합니다.
-              }
+        });
 
-              loader.hide();
-              content.show();
-          }).catch(function(error) {
-              console.warn(error);
-          });
-      },
 
-      
-      /*
+
+        // Load Contract data
+
+        App.contracts.Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            return electionInstance.candidatesCount();
+        }).then(function(candidatesCount) {
+            var candidatesResults = $("#candidatesResults");
+            candidatesResults.empty();
+            // candidatesResult를 index.html에 표시. 
+            var candidatesSelect = $("#candidatesSelect");
+            candidatesSelect.empty();
+            for (var i = 1; i <= candidatesCount; i++) {
+
+                electionInstance.candidates(i).then(function(candidate) {
+                    var id = candidate[0];
+                    var name = candidate[1];
+                    var voteCount = candidate[2];
+
+
+                    // 후보자 수 만큼 electionInstance에 후보자 데이터를 저장
+                    // Render candidate Result
+                    var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+                    candidatesResults.append(candidateTemplate);
+                    // 후보자 템플릿에 id, name, votecount를 추가
+
+                    // Render Candidate ballot option
+                    var candidateOption = "<option value='" + id + "' >" + name + "</option>"
+                    candidatesSelect.append(candidateOption);
+
+                    //Render candidate Result total
+                });
+            }
+            return electionInstance.voters(App.account);
+        }).then(function(hasVoted) {
+            //Do not allow a user to Vote
+            if (hasVoted) {
+                $('form').hide();
+
+                // 투표를 한 경우 폼을 삭제합니다.
+            }
+
+
+            var day = new Date();
+            var hour = day.getHours();
+            var minute = day.getMinutes();
+            loader.hide();
+            content.show();
+            if (hour > 24 && minute > 00) {
+                content.hide();
+                alert("투표는 종료되었습니다");
+            }
+            loader.hide();
+            content.show();
+
+
+        }).catch(function(error) {
+            console.warn(error);
+        });
+    },
+
+
+    /*
             // Listen for events emitted from the contract
             listenForEvents: function() {
                 App.contracts.Election.deployed().then(function(instance) {
@@ -133,56 +144,55 @@
                 })
             },
     */
-        
-
-      castVote: function() {
-          var candidateId = $('#candidatesSelect').val();
-          App.contracts.Election.deployed().then(function(instance) {
-              return instance.vote(candidateId, { from: App.account });
-          }).then(function(result) {
-              // Wait for votes to update
-              alert("투표가 완료되었습니다");
-              $("#content").hide();
-              $("#loader").show();
-
-          }).catch(function(err) {
-              console.error(err);
-          });
-      },
 
 
-      renderStore : function() {
-          //Get the Product Count
-          // Loop through and fetch all products by Id
-           var instance;
-          return App.contracts.Election.deployed().then(function(instance){
-              return instance.productIndex.call();             
-          }).then(function(count) {
-             for(var i=1; i<=count; i++) {
-                  App.renderProduct(instance, i);
-              }
-          });
-        },
+    castVote: function() {
+        var candidateId = $('#candidatesSelect').val();
+        App.contracts.Election.deployed().then(function(instance) {
+            return instance.vote(candidateId, { from: App.account });
+        }).then(function(result) {
+            // Wait for votes to update
+            alert("투표가 완료되었습니다");
+            $("#content").hide();
+            $("#loader").show();
 
-        renderProduct : function(instance, index) {
-            App.contracts.Election.deployed().then(function(instance) {
-              return instance.getProduct.call(index);
-            }).then(function(f){
-              let node = $("#electionName");
-              node.append(f[1]);
-           
-          });
-          
-        }
+        }).catch(function(err) {
+            console.error(err);
+        });
+    },
 
 
+    renderStore: function() {
+        //Get the Product Count
+        // Loop through and fetch all products by Id
+        var instance;
+        return App.contracts.Election.deployed().then(function(instance) {
+            return instance.productIndex.call();
+        }).then(function(count) {
+            for (var i = 1; i <= count; i++) {
+                App.renderProduct(instance, i);
+            }
+        });
+    },
 
-  }
+    renderProduct: function(instance, index) {
+        App.contracts.Election.deployed().then(function(instance) {
+            return instance.getProduct.call(index);
+        }).then(function(f) {
+            let node = $("#electionName");
+            node.append(f[1]);
 
-  $(function() {
-      $(window).load(function() {
-          App.init();
-       
-      });
-  });
+        });
 
+    }
+
+
+
+}
+
+$(function() {
+    $(window).load(function() {
+        App.init();
+
+    });
+});
--- election/src/js/load.js
+++ election/src/js/load.js
@@ -3,7 +3,7 @@
       web3Provider: null,
       contracts: {},
       account: '0x0',
-       
+
       init: function() {
           return App.initWeb3();
       },
@@ -42,7 +42,7 @@
       render: function() {
           var instance;
           var content = $("#main");
- 
+
           // index.html의 loader와 content와 연결
 
           content.show();
@@ -59,71 +59,70 @@
 
           });
 
-         
+
           $("#add-item-to-store").submit(function(event) {
               const req = $("#add-item-to-store").serialize();
-                let params = JSON.parse('{"' + req.replace(/"/g, '\\"').replace(/&/g,'","').replace(/=/g,'":"') +
-              '"}');
+              let params = JSON.parse('{"' + req.replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') +
+                  '"}');
               let decodedParams = {}
-              Object.keys(params).forEach(function(v){
+              Object.keys(params).forEach(function(v) {
                   decodedParams[v] = decodeURIComponent(decodeURI(params[v]));
               });
-             
-              console.log(decodedParams); 
+
+              console.log(decodedParams);
               App.saveProduct(decodedParams);
-              event.preventDefault();  
+              event.preventDefault();
 
           });
-         App.renderStore();
+          App.renderStore();
       },
 
-      saveProduct : function(product) {
-          App.contracts.Election.deployed().then(function(f){
-              return f.addProductToStore(product["product-name"], product["product-candidateOne"], product["product-candidateTwo"], 
-              Date.parse(product["product-start-time"]) / 1000, Date.parse(product["product-end-time"]) / 1000,
-              product["product-condition"], {from :  web3.eth.accounts[0], gas: 4700000});    
+      saveProduct: function(product) {
+          App.contracts.Election.deployed().then(function(f) {
+              return f.addProductToStore(product["product-name"], product["product-candidateOne"], product["product-candidateTwo"],
+                  Date.parse(product["product-start-time"]) / 1000, Date.parse(product["product-end-time"]) / 1000,
+                  product["product-condition"], { from: web3.eth.accounts[0], gas: 4700000 });
           }).then(function(f) {
               alert("성공적으로 등록되었습니다.");
           });
       },
 
-      renderStore : function() {
+      renderStore: function() {
           //Get the Product Count
           // Loop through and fetch all products by Id
-           var instance;
-          return App.contracts.Election.deployed().then(function(instance){
-              return instance.productIndex.call();             
+          var instance;
+          return App.contracts.Election.deployed().then(function(instance) {
+              return instance.productIndex.call();
           }).then(function(count) {
-              for(var i=1; i<=count; i++) {
+              for (var i = 1; i <= count; i++) {
                   App.renderProduct(instance, i);
               }
           });
-        },
+      },
 
-        renderProduct : function(instance, index) {
-            App.contracts.Election.deployed().then(function(instance) {
+      renderProduct: function(instance, index) {
+          App.contracts.Election.deployed().then(function(instance) {
               return instance.getProduct.call(index);
-            }).then(function(f){
+          }).then(function(f) {
               let node = $(".databox");
               node.append("<div class='voteData'>" +
-                        "<div class='infoElection__title'> 투표 제목 : " + f[1] + "</div>" 
-                        + "<div class='infoElection__title'> 1번 후보 : " + f[2] + "</div>" +
-                         "<div class='infoElection__title'> 2번 후보 : " + f[3] + "</div>" +
-                        "<div class='infoElection__title'> 투표 기간 " + f[4] + "~" + f[5] + "</div>" +
-                        "<button class='vote__btn' ><a href='election.html'>투표 하기</a></button>" +  "</div>" );
+                  "<div class='infoElection__title'> 투표 제목 : " + f[1] + "</div>" +
+                  "<div class='infoElection__title'> 1번 후보 : " + f[2] + "</div>" +
+                  "<div class='infoElection__title'> 2번 후보 : " + f[3] + "</div>" +
+                  //   "<div class='infoElection__title'> 투표 기간 " + f[4] + "~" + f[5] + "</div>" +
+                  "<button class='vote__btn goto' ><a href='election.html'>투표 하기</a></button>" + "</div>");
 
-             console.log(f);
+              console.log(f);
           });
-          
-        }
-        
+
+      }
+
   }
 
 
-  
+
   $(function() {
       $(window).load(function() {
           App.init();
       });
   });
-
